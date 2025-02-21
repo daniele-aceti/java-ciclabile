95e90279c6c86e5f17c7d521a5d7b1a7a10b07a2
daniele-aceti <daniele.aceti@hotmail.it>
2025-02-21 11:34:31 +0100

    fix: code

--- src/App.java
+++ src/App.java
@@ -4,16 +4,10 @@ public class App {
     public static void main(String[] args) {
         int[] a = {1, 2, 3, 4, 5, 67, 8, 65};
         Ciclabile array = new Ciclabile(a);
-        System.out.println(array.getElementoSuccessivo());
-        System.out.println(array.getElementoSuccessivo());
-        System.out.println(array.getElementoSuccessivo());
-        System.out.println(array.getElementoSuccessivo());
-        System.out.println(array.getElementoSuccessivo());
-        System.out.println(array.getElementoSuccessivo());
-        System.out.println(array.getElementoSuccessivo());
-        System.out.println(array.hasAncoraElementi());
-        System.out.println(array.getElementoSuccessivo());
-        System.out.println(array.getElementoSuccessivo());
+        for( int i = 0; i < 10; i++){
+            System.out.println(array.getElementoSuccessivo());
+
+        }
         System.out.println(array.hasAncoraElementi());
 
         Ciclabile array2 = new Ciclabile();
